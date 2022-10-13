07a85e6023579cf89b14411faebded47ebbc33e7
Your Name <you@example.com>
2022-10-13 16:31:16 +0700

    first commit

--- index.js
+++ index.js
@@ -19,13 +19,11 @@ const notFound = require('./middlewares/notFound')
 const error = require('./middlewares/error')
 
 const app = express()
-
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-
 // app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
