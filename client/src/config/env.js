489744f6fd35933a03e812ef7f083a45fa50c59a
Your Name <you@example.com>
2022-10-14 06:56:07 +0700

    commit

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,3 +1,5 @@
 export const API_ENDPOINT_URL = 'https://tell-your-pharmacist.herokuapp.com/'
 
 export const GOOGLE_API_KEY = 'AIzaSyChssTkrahUAiNHmoFX7HevFtXigmE-5Zo'
+
+export const STRIPE_KEY = 'pk_test_51Lmc5pBmFg3X0UhhmE1titsb4V2v74qbKqqUJ6zcjPRpUajWpIM3MrAtPkYGepQGnEwZUEQ00TbQKp7DAHYf6WNa00y8X3ZtQ8'
