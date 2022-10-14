33f8f9dbed686b380b511b9bcf5f0d512fc0ed77
Your Name <you@example.com>
2022-10-14 08:46:12 +0700

    commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use("/reviews", reviewRoute);
 app.use("/openingTime", openingTimeRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("/client/dist"));
+    app.use(express.static(path.join(__dirname, "/client/dist")));
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
     });
