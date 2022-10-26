cfe7cd4d710bf1e02382bc40080c18a861022661
Your Name <you@example.com>
2022-10-26 13:56:09 +0700

    commit

--- client/src/config/env.js
+++
@@ -1,6 +0,0 @@
-export const API_ENDPOINT_URL = 'http://localhost:8000'
-
-export const GOOGLE_API_KEY = 'AIzaSyChssTkrahUAiNHmoFX7HevFtXigmE-5Zo'
-
-export const STRIPE_KEY = 'pk_test_51Lmc5pBmFg3X0UhhmE1titsb4V2v74qbKqqUJ6zcjPRpUajWpIM3MrAtPkYGepQGnEwZUEQ00TbQKp7DAHYf6WNa00y8X3ZtQ8'
-
